a784c6ab306aeed1cb3c6645dd7e9d577e7dd69d
Aleksandr SaPozhkov <alexander@sapozhkov.net>
2022-02-23 00:09:21 +0100

    Delete .devcontainer directory

--- .devcontainer/spfx-startup.sh
+++
@@ -1,8 +0,0 @@
-echo
-echo -e "\e[1;94mInstalling Node dependencies\e[0m"
-npm install
-
-## commands to create dev certificate and copy it to the root folder of the project
-echo
-echo -e "\e[1;94mGenerating dev certificate\e[0m"
-gulp trust-dev-cert
